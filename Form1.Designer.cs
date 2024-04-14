28cecd8632f288e773a6dcb284d96ae2b91f7b93
User <User@LAPTOP-7GGL8BL1>
2024-04-14 20:06:36 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace OpSis8_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(95, 85);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.TabIndex = 0;
+            this.buttonRead.Text = "Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.Location = new System.Drawing.Point(95, 158);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(75, 23);
+            this.buttonWrite.TabIndex = 1;
+            this.buttonWrite.Text = "Write";
+            this.buttonWrite.UseVisualStyleBackColor = true;
+            this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(95, 230);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(306, 76);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 177);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonWrite);
+            this.Controls.Add(this.buttonRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Button buttonWrite;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+
+namespace OpSis8_1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            string keyName = "Software\\MyRegistryKey";
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(keyName);
+            if (key != null)
+            {
+                string value = key.GetValue("ValueName") as string;
+                MessageBox.Show("Значение, считанное из реестра: " + value);
+            }
+            else
+            {
+                MessageBox.Show("Значение реестра не найдено");
+            }
+        }
+
+        private void buttonWrite_Click(object sender, EventArgs e)
+        {
+            string keyName = "Software\\MyRegistryKey";
+            string valueName = "ValueName";
+            string value = textBox1.Text;
+
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(keyName);
+            key.SetValue(valueName, value);
+            key.Close();
+
+            MessageBox.Show("Значение, записанное в реестр");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            string keyName = "Software\\MyRegistryKey";
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(keyName, true);
+            if (key != null)
+            {
+                key.DeleteValue("ValueName");
+                key.Close();
+                MessageBox.Show("Значение удалено из реестра");
+            }
+            else
+            {
+                MessageBox.Show("Значение реестра не найдено");
+            }
+        }
+    }
+}
+
+
